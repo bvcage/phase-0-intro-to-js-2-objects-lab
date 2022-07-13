69f7d2e6996e49164ab9f83da2c946b1439d422d
Bailey Cage <bailey.cage@gmail.com>
2022-07-13 19:15:12 -0400

    commit tree eb2352e03382d8c5bfed010d70bc20ca512bd45e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]: value};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const returnObj = {...employee};
+    delete returnObj[key];
+    return returnObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+const employee = {
+    name: 'Bailey',
+    streetAddress: '123 Main Street',
+}
